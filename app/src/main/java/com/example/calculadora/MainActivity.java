4e9482c683752c989a2b3a4c8d4011793978c0b8
Ana <almendras451@gmail.com>
2023-06-16 22:42:58 -0400

    ejercicio 4

--- app/src/main/java/com/example/calculadora/MainActivity.java
+++ app/src/main/java/com/example/calculadora/MainActivity.java
@@ -2,7 +2,12 @@ package com.example.calculadora;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.SyncStatusObserver;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,86 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        EditText numero1 = findViewById(R.id.numero1editText);
+        EditText numero2 = findViewById(R.id.numero2EditText);
+
+        Button sumaB = findViewById(R.id.sumaButton);
+       sumaB.setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View v) {
+               calcular("+",numero1,numero2);
+           }
+       });
+
+        Button restaB = findViewById(R.id.restaButton);
+        restaB.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+            calcular("-",numero1,numero2);
+            }
+        });
+
+        Button divicionB = findViewById(R.id.divicionButton);
+        divicionB.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                calcular("/",numero1,numero2);
+            }
+        });
+
+        Button multiplicacionB =  findViewById(R.id.multiplicarButton);
+        multiplicacionB.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                calcular("*",numero1,numero2);
+            }
+        });
+
+
+
     }
+
+    private void calcular(String signo, EditText numb1EditTxT, EditText numb2EditTxT) {
+
+        float resultado=0;
+
+
+
+        if(numb1EditTxT.getText().toString().isEmpty() || numb2EditTxT.getText().toString().isEmpty()){
+            Toast.makeText(getBaseContext(),"Rellenar los campos vacios.", Toast.LENGTH_LONG).show();
+        }else {
+            Integer numb1 = Integer.parseInt(numb1EditTxT.getText().toString());
+            Integer numb2 = Integer.parseInt(numb2EditTxT.getText().toString());
+
+            if(signo.equals("/")&& numb1 == 0 ){
+                Toast.makeText(getBaseContext(),  "no se puede dividir por 0: " , Toast.LENGTH_SHORT).show();
+
+            }else {
+            switch (signo) {
+            case "+":
+                resultado = numb1 + numb2;
+                break;
+            case "-":
+                resultado = numb1 - numb2;
+                break;
+            case "/":
+                    resultado = numb1 / numb2;
+                break;
+
+            case "*":
+                resultado = numb1 * numb2;
+                break;
+            }
+
+                String mensaje = numb1 + " " + signo + " " + numb2+ " = " + resultado;
+
+                Toast.makeText(getBaseContext(),mensaje, Toast.LENGTH_SHORT).show();
+
+
+            }
+        }
+    }
+
+
 }
